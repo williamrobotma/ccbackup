f20cd9974e782bb7ca555d10cefb773f6c60cd3f
William Ma <mawilliam.v20@gmail.com>
2025-05-30 23:33:14 -0400

    finalized compression

--- compress_job.sh
+++ compress_job.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=2
+# #SBATCH --cpus-per-task=2
 
 set -x
 
 start=`date +%s`
 
 cd "${origin_dir}"
-tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" -cvzf "${dest_targz_path}" "${origin_basename}"
-
+if [ $compress == 1 ]; then
+    tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" --exclude="__pycache__" -I "zstd -8 -T0" -cvf "${dest_targz_path}" "${origin_basename}"
+else
+    tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" --exclude="__pycache__" -cvf "${dest_targz_path}" "${origin_basename}"
+fi
 
 end=`date +%s`
 echo "script time: $(($end-$start))"
