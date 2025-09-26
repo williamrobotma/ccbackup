775729f74678c26373850c3d47e235cb410b2fdd
William Ma <mawilliam.v20@gmail.com>
2025-09-25 21:38:55 -0400

    made it backup .git folder

--- compress_job.sh
+++ compress_job.sh
@@ -7,9 +7,11 @@ start=`date +%s`
 
 cd "${origin_dir}"
 if [ $compress == 1 ]; then
-    tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" --exclude="__pycache__" -I "zstd -8 -T0" -cvf "${dest_targz_path}" "${origin_basename}"
+    # tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" --exclude="__pycache__" -I "zstd -8 -T0" -cvf "${dest_targz_path}" "${origin_basename}"
+    tar --exclude="${origin_basename}/.venv" --exclude="__pycache__" -I "zstd -8 -T0" -cvf "${dest_targz_path}" "${origin_basename}"
 else
-    tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" --exclude="__pycache__" -cvf "${dest_targz_path}" "${origin_basename}"
+    # tar --exclude="${origin_basename}/.git" --exclude="${origin_basename}/.venv" --exclude="__pycache__" -cvf "${dest_targz_path}" "${origin_basename}"
+    tar --exclude="${origin_basename}/.venv" --exclude="__pycache__" -cvf "${dest_targz_path}" "${origin_basename}"
 fi
 
 end=`date +%s`
